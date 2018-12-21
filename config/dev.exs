a7c954ecad14951bec847f0be95f4bcd7244ac41
Lukas N <niederbergerlukas@gmail.com>
2018-12-21 13:53:03 +0100

    Removed default config, should be set by consumer.

--- config/dev.exs
+++ config/dev.exs
@@ -1,10 +1,1 @@
 use Mix.Config
-
-config :ueberauth, Ueberauth.Strategy.EVESSO.OAuth,
-  client_id: System.get_env("EVE_SSO_CLIENT_ID"),
-  client_secret: System.get_env("EVE_SSO_CLIENT_SECRET")
-
-config :oauth2,
-  serializers: %{
-    "application/json" => Jason
-  }
